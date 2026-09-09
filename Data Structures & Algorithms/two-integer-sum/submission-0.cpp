1e290b69a7bfd0255214d4a3a88a5eec08dc33a6
Mustafa Ansary <mustafa.ansari0012@gmail.com>
2026-09-09 17:42:04 +0530

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        map<int, int> hash;
+        for (int i=0; i<nums.size(); i++) {
+            int diff = target - nums[i];
+            auto it = hash.find(diff);
+            if (it != hash.end())
+                return vector<int> {it->second, i};
+            hash[nums[i]] = i;
+        }
+    }
+};
